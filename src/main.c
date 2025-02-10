4734092a276a00cb3bbc126b6edc1722133ff73d
SwapnilAkhade824 <swapnilsantoshakhade@gmail.com>
2025-02-10 18:29:57 +0530

    -

--- src/main.c
+++ src/main.c
@@ -3,30 +3,97 @@
 #include <math.h>
 #include <raylib.h>
 #include <string.h>
+#include <stdlib.h>
+// #include <ctype.h>
+
+#define RAYGUI_IMPLEMENTATION
+#include<raygui.h>
 
 // Constants
-#define SCREEN_WIDTH  800
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH  1200
+#define SCREEN_HEIGHT 800
+
+
+struct Account {
+    long long accno;
+    char username[10];
+    char password[10];
+};
+
 
 int main(int argc, char const *argv[]) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Creast Banking");
     SetTargetFPS(60);
 
-    Texture2D Logo = LoadTexture("image\\logo.png");
+    Texture2D Logo = LoadTexture("image/logo.png");
+
+    Font customFont = LoadFontEx("resources/Roboto-Regular.ttf", 64, 0, 0);
+    GenTextureMipmaps(&customFont.texture);
+    SetTextureFilter(customFont.texture, TEXTURE_FILTER_BILINEAR);
+
+    if (customFont.texture.id == 0) {
+        printf("Failed to load font!\n");
+        CloseWindow();
+        return -1;
+    }
+
+    const char* intro = "Welcome to the CREST Bank";
 
     while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        char intro[100] = "Welcome to the CREAST Bank";
-        DrawText(intro, (800 - MeasureText(intro, 30)) / 2, 50, 30, BLACK);
 
-        DrawTexture(Logo, 80, 50, WHITE);
+        Color peach = { 255, 218, 185, 255 };  // Peach color (Soft warm tone)
+
+        BeginDrawing();
+        ClearBackground(peach);
+
+        // data of user
+        struct Account userdata;
+
+        bool editMode = false;
+
+        // input values
+        char inputaccno[13]="";
+        char inputpassword[11]="";
+
+        Vector2 intro_textsize = MeasureTextEx(customFont, intro, 50, 2);
+        Vector2 intro_position = {(SCREEN_WIDTH - intro_textsize.x)/2,(225 - intro_textsize.y)/2};
+
+        // text of intro
+
+        DrawTextEx(customFont, intro, intro_position, 50, 2, BLACK);
+
+        // logo of the app
+
+        DrawTexture(Logo, (SCREEN_WIDTH - Logo.width) / 2, 125, WHITE);
+
+        // text or input box
+
+        Rectangle accno = { (SCREEN_WIDTH - 600)/2, SCREEN_HEIGHT/8*5, 600, 60 };
+        DrawRectangleRounded(accno, 0.3, 10, LIGHTGRAY);
+
+        char *accno_placeholder = "Enter the Account no or Username";
+        Vector2 accno_textsize = MeasureTextEx(customFont, accno_placeholder, 25, 2);
+        Vector2 accno_position = {(SCREEN_WIDTH - accno_textsize.x)/2,accno.y + (accno.height - accno_textsize.y)/2};
+
+        DrawTextEx(customFont, accno_placeholder, accno_position, 25, 2, BLACK);
+
+        Rectangle password = { (SCREEN_WIDTH - 600)/2, SCREEN_HEIGHT/8*5 + 120, 600, 60 };
+        DrawRectangleRounded(password, 0.3, 10, LIGHTGRAY);
+
+        char *password_placeholder = "Enter your Password";
+        Vector2 password_textsize = MeasureTextEx(customFont, password_placeholder, 25, 2);
+        Vector2 password_position = {(SCREEN_WIDTH - password_textsize.x)/2,password.y + (password.height - password_textsize.y)/2};
+
+        DrawTextEx(customFont, password_placeholder, password_position, 25, 2, BLACK);
 
         EndDrawing();
     }
 
     UnloadTexture(Logo);
+    UnloadFont(customFont);
 
     CloseWindow();
     return 0;
 }
+
+
